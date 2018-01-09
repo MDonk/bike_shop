b54ddc7b6217b46e2d38096aca8797a6381c52b9
MDonk <mdonkin_pt@hotmail.com>
2018-01-09 11:45:50 +1100

    Update bicycles.py

--- bicycles.py
+++ bicycles.py
@@ -1,25 +1,26 @@
-class bicycle(object):
+class bicycle:
     def __init__(self, cost):
         self.cost = cost
 
-    def sale_price(self):
-        for self.cost in cost:
-            self.cost += 10
+    def sale_price(self, cost):
+        self.cost = cost
+        for value in self.cost:
+            value += 10
 
-class customer(object):
+class customer:
     def __init__(self, budget):
         self.budget = budget
             
-red = bicycle([700])
-blue = bicycle([800])
-black = bicycle([400])
-green = bicycle([300])
-pink = bicycle([100])
-yellow = bicycle([120])
+red = bicycle(700)
+blue = bicycle(800)
+black = bicycle(400)
+green = bicycle(300)
+pink = bicycle(100)
+yellow = bicycle(120)
 
-Tim = customer([200])
-Jack = customer([500])
-Tom = customer([1000])
+Tim = customer(200)
+Jack = customer(500)
+Tom = customer(1000)
 
-bikes = bicycle(object)
+bikes = bicycle()
 bikes.sale_price()
